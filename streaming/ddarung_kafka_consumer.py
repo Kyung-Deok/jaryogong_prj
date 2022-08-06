6a1568fe7ccdfc20babae025769ed7f4d04d1cc5
Kyung-Deok <cftcom@naver.com>
2022-08-07 03:51:48 +0900

    producer/consumer

---
+++ streaming/ddarung_kafka_consumer.py
@@ -0,0 +1,20 @@
+from kafka import KafkaConsumer
+from json import loads
+
+# topic, broker list
+consumer = KafkaConsumer(
+    'stream_bikes',
+     bootstrap_servers=['ubuntu:9091', 'ubuntu:9092', 'ubuntu:9093'],
+     auto_offset_reset='earliest',
+     enable_auto_commit=True,
+     value_deserializer=lambda x: loads(x.decode('utf-8')),
+     consumer_timeout_ms=1000
+)
+
+# consumer list를 가져온다
+print('[begin] get consumer list')
+for message in consumer:
+    print("Topic: %s, Partition: %d, Offset: %d, Key: %s, Value: %s" % (
+        message.topic, message.partition, message.offset, message.key, message.value
+    ))
+print('[end] get consumer list')
