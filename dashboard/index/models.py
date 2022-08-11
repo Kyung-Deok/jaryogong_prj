a8115d0b27cdb2050724395571e3fc2c8f258957
Kyung-Deok <cftcom@naver.com>
2022-08-12 04:40:36 +0900

    220812/kdlee

--- dashboard/index/models.py
+++ dashboard/index/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+# rental_per_year : 연도별 대여량
+
+
+# rental_record_per_hour : 시간대 별 대여량 as rrph
+
+
+# bike_rental : 대여 정보(시간대 별 대여소 별 이용량) as br
+
+
+
+# bike_rental : 대여 정보(팩트 테이블)
